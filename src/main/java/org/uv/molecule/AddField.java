ef946ee1c8915f9399e54d2e9d37b5b9481c6337
andres castillo <andcastillo@gmail.com>
2022-01-25 16:40:12 -0500

    many fixes

--- src/main/java/org/uv/molecule/AddField.java
+++ src/main/java/org/uv/molecule/AddField.java
@@ -1,22 +1,16 @@
 package org.uv.molecule;
 
-import java.io.*;
-
-import org.openscience.cdk.AtomContainer;
-import org.openscience.cdk.exception.CDKException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.io.MDLV2000Reader;
-import org.openscience.cdk.interfaces.IChemObjectBuilder;
 import org.openscience.cdk.io.MDLV3000Reader;
-import org.openscience.cdk.io.iterator.IteratingSDFReader;
-import org.openscience.cdk.DefaultChemObjectBuilder;
-import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.qsar.descriptors.molecular.HBondAcceptorCountDescriptor;
 import org.openscience.cdk.qsar.descriptors.molecular.HBondDonorCountDescriptor;
-import org.json.*;
 
-import java.nio.charset.StandardCharsets;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
@@ -54,44 +48,37 @@ public class AddField {
 
                 while(substancesIterator.hasNext()) {
                     JSONObject row = (JSONObject) substancesIterator.next();
-                    JSONObject calc = row.getJSONObject("CALC");
-                    int xrn = row.getJSONObject("IDE").getJSONObject("IDE.XRN").getInt("content");
-                    JSONObject substance = null;
-                    if (row.get("YY").getClass() == JSONArray.class)  {
-                        substance = row.getJSONArray("YY").getJSONObject(0).getJSONObject("YY.STR");
-                    } else {
-                        substance = row.getJSONObject("YY").getJSONObject("YY.STR");
-                    }
-                    String rn = null;
-                    if (row.getJSONObject("IDE").has("IDE.RN")) {
-                        if (row.getJSONObject("IDE").get("IDE.RN").getClass() == JSONArray.class) {
-                            rn = row.getJSONObject("IDE").getJSONArray("IDE.RN").getString(0);
+                    if (row.getJSONObject("IDE").getJSONObject("IDE.MF").optString("content").contains("\"C\"")) {
+                        JSONObject calc = row.getJSONObject("CALC");
+                        int xrn = row.getJSONObject("IDE").getJSONObject("IDE.XRN").getInt("content");
+                        JSONObject substance = null;
+                        if (row.get("YY").getClass() == JSONArray.class)  {
+                            substance = row.getJSONArray("YY").getJSONObject(0).getJSONObject("YY.STR");
                         } else {
-                            rn = row.getJSONObject("IDE").optString("IDE.RN", xrn + "");
+                            substance = row.getJSONObject("YY").getJSONObject("YY.STR");
                         }
-                    } else {
-                        rn = xrn + "";
-                    }
-                    String molFile = "\n" + substance.getString("content");
-                    MDLV3000Reader mdl = new MDLV3000Reader( new ByteArrayInputStream(molFile.getBytes()));
-                    try {
-                        IAtomContainer container = mdl.read(new org.openscience.cdk.AtomContainer(0, 0, 0, 0));
-                        HBondDonorCountDescriptor calculator = new HBondDonorCountDescriptor();
-                        org.openscience.cdk.qsar.DescriptorValue value = calculator.calculate(container);
-                        String output = xrn + " " + rn + " ";
-                        output += calc.getFloat("CALC.TPSA") + " ";
-                        output += calc.getFloat("CALC.ROTBND") + " ";
-                        //output += calc.getFloat("CALC.HDONOR") + " ";
-                        output += calc.getFloat("CALC.VEBER") + " ";
-                        output += calc.getFloat("CALC.LOGP") + " ";
-                        output += calc.getFloat("CALC.HACCOR") + " ";
-                        output += calc.getFloat("CALC.LIPINSKI") + " ";
-                        output += value.getValue();
+                        String rn = "ND";
+                        if (row.getJSONObject("IDE").has("IDE.RN"))
+                            rn = row.getJSONObject("IDE").optString("IDE.RN", "ND");
+                        String molFile = "\n" + substance.getString("content");
+                        MDLV3000Reader mdl = new MDLV3000Reader( new ByteArrayInputStream(molFile.getBytes()));
+                        try {
+                            IAtomContainer container = mdl.read(new org.openscience.cdk.AtomContainer(0, 0, 0, 0));
+                            HBondAcceptorCountDescriptor calculator = new HBondAcceptorCountDescriptor();
+                            org.openscience.cdk.qsar.DescriptorValue value = calculator.calculate(container);
+                            String output = xrn + " " + rn + " ";
+                            output += row.getJSONObject("IDE").getInt("IDE.MW") + "\t";
+                            output += calc.getFloat("CALC.LOGP") + "\t";
+                            output += calc.getFloat("CALC.HDONOR") + "\t";
+                            output += calc.getFloat("CALC.HACCOR") + "\t";
+                            output += value.getValue() + "\t";
+                            output += calc.getFloat("CALC.ROTBND") + "\t";
+                            output += calc.getFloat("CALC.TPSA") + "\t";
+                            System.out.println(output);
+                        } catch (Exception e) {
+                            //System.out.println("xx : " + e);
 
-                        System.out.println(output);
-                    } catch (Exception e) {
-                        //System.out.println("xx : " + e);
-
+                        }
                     }
                 }
             }
