afcb45b3b09e59e5fd0ebab87056d7042233299e
andres castillo <andcastillo@gmail.com>
2022-02-03 19:03:36 -0500

    many if fixes and some try catch

--- src/main/java/org/uv/molecule/AddField.java
+++ src/main/java/org/uv/molecule/AddField.java
@@ -48,45 +48,48 @@ public class AddField {
                 Iterator substancesIterator = substances.iterator();
 
                 while(substancesIterator.hasNext()) {
-                    JSONObject row = (JSONObject) substancesIterator.next();
-                    if (row.getJSONObject("IDE").getJSONObject("IDE.MF").optString("content").contains("\"C\"")) {
-                        JSONObject calc = row.getJSONObject("CALC");
-                        int xrn = row.getJSONObject("IDE").getJSONObject("IDE.XRN").getInt("content");
-                        JSONObject substance = null;
-                        if (row.get("YY").getClass() == JSONArray.class)  {
-                            substance = row.getJSONArray("YY").getJSONObject(0).getJSONObject("YY.STR");
-                        } else {
-                            substance = row.getJSONObject("YY").getJSONObject("YY.STR");
-                        }
-                        String rn = "ND";
-                        if (row.getJSONObject("IDE").has("IDE.RN"))
-                            rn = row.getJSONObject("IDE").optString("IDE.RN", "ND");
-                        String molFile = substance.getString("content");
-                        try {
-                            DefaultChemObjectReader mdl;
-                            if (molFile.contains("V2000")) {
-                                mdl = new MDLV2000Reader(new ByteArrayInputStream(molFile.getBytes()));
-                            }
-                            else {
-                                mdl = new MDLV3000Reader( new ByteArrayInputStream(( "\n" + molFile).getBytes()));
-                            }
+                    try {
+                        JSONObject row = (JSONObject) substancesIterator.next();
+                        if (row.has("IDE") && row.getJSONObject("IDE").has("IDE.MF") &&
+                                row.getJSONObject("IDE").getJSONObject("IDE.MF").optString("content").contains("\"C\"")) {
+                            JSONObject calc = row.optJSONObject("CALC");
+                            if (calc != null) {
+                                    int xrn = row.getJSONObject("IDE").getJSONObject("IDE.XRN").getInt("content");
+                                    JSONObject substance = null;
+                                    if (row.get("YY").getClass() == JSONArray.class)  {
+                                        substance = row.getJSONArray("YY").getJSONObject(0).getJSONObject("YY.STR");
+                                    } else {
+                                        substance = row.getJSONObject("YY").getJSONObject("YY.STR");
+                                    }
+                                    String rn = "ND";
+                                    if (row.getJSONObject("IDE").has("IDE.RN"))
+                                        rn = row.getJSONObject("IDE").optString("IDE.RN", "ND");
+                                    String molFile = substance.getString("content");
 
-                            IAtomContainer container = mdl.read(new org.openscience.cdk.AtomContainer(0, 0, 0, 0));
-                            HBondAcceptorCountDescriptor calculator = new HBondAcceptorCountDescriptor();
-                            org.openscience.cdk.qsar.DescriptorValue value = calculator.calculate(container);
-                            String output = xrn + "\t\"" + rn + "\"\t";
-                            output += row.getJSONObject("IDE").getInt("IDE.MW") + "\t";
-                            output += calc.getFloat("CALC.LOGP") + "\t";
-                            output += calc.getFloat("CALC.HDONOR") + "\t";
-                            output += calc.getFloat("CALC.HACCOR") + "\t";
-                            output += value.getValue() + "\t";
-                            output += calc.getFloat("CALC.ROTBND") + "\t";
-                            output += calc.getFloat("CALC.TPSA") + "\t";
-                            System.out.println(output);
-                        } catch (Exception e) {
-                            //System.out.println("xx : " + e);
+                                    DefaultChemObjectReader mdl;
+                                    if (molFile.contains("V2000")) {
+                                        mdl = new MDLV2000Reader(new ByteArrayInputStream(molFile.getBytes()));
+                                    }
+                                    else {
+                                        mdl = new MDLV3000Reader( new ByteArrayInputStream(( "\n" + molFile).getBytes()));
+                                    }
 
+                                    IAtomContainer container = mdl.read(new org.openscience.cdk.AtomContainer(0, 0, 0, 0));
+                                    HBondAcceptorCountDescriptor calculator = new HBondAcceptorCountDescriptor();
+                                    org.openscience.cdk.qsar.DescriptorValue value = calculator.calculate(container);
+                                    String output = xrn + "\t\"" + rn + "\"\t";
+                                    output += row.getJSONObject("IDE").getInt("IDE.MW") + "\t";
+                                    output += calc.optFloat("CALC.LOGP", -999) + "\t";
+                                    output += calc.optFloat("CALC.HDONOR", -999) + "\t";
+                                    output += calc.optFloat("CALC.HACCOR", -999) + "\t";
+                                    output += value.getValue() + "\t";
+                                    output += calc.optFloat("CALC.ROTBND", -999) + "\t";
+                                    output += calc.optFloat("CALC.TPSA", -999) + "\t";
+                                    System.out.println(output);
+                            }
                         }
+                    } catch (Exception e) {
+                        //System.out.println("xx : " + e);
                     }
                 }
             }
